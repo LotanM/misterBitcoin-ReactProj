9216ab334409fe09727fc1405ceb23bbe225b96c
Lotan <lotan.mash@gmail.com>
2021-04-05 14:55:09 +0300

    first commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+
+import { Component } from 'react';
+
+export class HomePage extends Component {
+    state = {
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Home Page!</h1>
+            </div>
+        )
+    }
+}
